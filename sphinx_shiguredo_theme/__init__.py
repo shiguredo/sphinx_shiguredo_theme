93da711c21b50038250be35b9df4aef6304130ed
Yuki Ito <yuito@shiguredo.jp>
2021-01-26 11:29:50 +0900

    デザインを調整する

--- sphinx_shiguredo_theme/__init__.py
+++ sphinx_shiguredo_theme/__init__.py
@@ -1,4 +1,20 @@
 from os import path
+from docutils import nodes
+from sphinx.transforms.post_transforms import SphinxPostTransform
+
+class TableWrapperTransform(SphinxPostTransform):
+    builder = ('html',)
+    default_priority = 500
+
+    def run(self, **kwargs):
+        for node in self.document.traverse(nodes.table):
+            table_wrapper = nodes.container()
+            table_wrapper['classes'] = ['table_wrapper']
+            pos = node.parent.index(node)
+            node.parent.insert(pos, table_wrapper)
+            node.parent.remove(node)
+            table_wrapper += node
 
 def setup(app):
     app.add_html_theme('sphinx_shiguredo_theme', path.abspath(path.dirname(__file__)))
+    app.add_post_transform(TableWrapperTransform)
