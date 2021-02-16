4f2e2e94769323a2225418825e6ab45730a84a9e
Yuki Ito <yuito@shiguredo.jp>
2021-02-16 14:35:36 +0900

    セクションタイトルをそのまま id に使うようにする拡張を追加する

--- sphinx_shiguredo_theme/__init__.py
+++ sphinx_shiguredo_theme/__init__.py
@@ -2,6 +2,26 @@ from os import path
 from docutils import nodes
 from sphinx.transforms.post_transforms import SphinxPostTransform
 
+def on_doctree_resolved(app, doctree, docname):
+    from docutils import nodes
+    # add hash-based node-ID to sections
+    mapping = {}
+    for node in doctree.traverse(nodes.section):
+        new_id = node.children[0].astext()
+        for node_id in node['ids']:
+            mapping[node_id] = new_id
+        node['ids'].insert(0, new_id)
+    # use hash-based node-IDs at local reference
+    for node in doctree.traverse(nodes.reference):
+        refid = node.get('refid')
+        if refid in mapping:
+            node['refid'] = mapping.get(refid)
+    # use hash-based node-IDs at toctrees
+    for _, toctree in app.env.tocs.items():
+        for node in toctree.traverse(nodes.reference):
+            if node.get('internal') and node.get('anchorname'):
+                node['anchorname'] = '#' + node.astext()
+
 class TableWrapperTransform(SphinxPostTransform):
     builder = ('html',)
     default_priority = 500
@@ -17,4 +37,5 @@ class TableWrapperTransform(SphinxPostTransform):
 
 def setup(app):
     app.add_html_theme('sphinx_shiguredo_theme', path.abspath(path.dirname(__file__)))
+    app.connect('doctree-resolved', on_doctree_resolved)
     app.add_post_transform(TableWrapperTransform)
