9d0e2376c3f25a335bc2b1833d04f5fcd7c8143f
Yuki Ito <yuito@shiguredo.jp>
2021-02-18 14:40:47 +0900

    パーマリンクを section タイトルにする処理を修正する

--- sphinx_shiguredo_theme/__init__.py
+++ sphinx_shiguredo_theme/__init__.py
@@ -1,19 +1,26 @@
 from os import path
+import re
+
 from docutils import nodes
 from sphinx.transforms.post_transforms import SphinxPostTransform
 
+def delete_html_escape_string(string):
+    escape_strings = ['<', '>', ' ', '　', '&', '"', "'"]
+    result = string
+    for s in escape_strings:
+        result = result.replace(s, '')
+    return result
+
 def on_doctree_resolved(app, doctree, docname):
-    from docutils import nodes
-    # create same value list
-    values = [node.children[0].astext() for node in doctree.traverse(nodes.section)]
-    same_value_counter = { v: 0 for v in set([v for v in values if 1 < values.count(v)])}
     # add hash-based node-ID to sections
+    section_text_list = [node.children[0].astext() for node in doctree.traverse(nodes.section)]
+    doctree_same_values = set([v for v in section_text_list if 1 < section_text_list.count(v)])
     mapping = {}
     for node in doctree.traverse(nodes.section):
         new_id = node.children[0].astext()
-        if new_id in same_value_counter.keys():
-            same_value_counter[new_id] += 1
-            new_id += "-" + str(same_value_counter[new_id])
+        if new_id in doctree_same_values:
+            new_id += '-{}'.format(node['ids'][-1])
+        new_id = delete_html_escape_string(new_id)
         for node_id in node['ids']:
             mapping[node_id] = new_id
         node['ids'].insert(0, new_id)
@@ -24,12 +31,18 @@ def on_doctree_resolved(app, doctree, docname):
             node['refid'] = mapping.get(refid)
     # use hash-based node-IDs at toctrees
     for _, toctree in app.env.tocs.items():
+        reference_text_list = [node.astext() for node in toctree.traverse(nodes.reference)]
+        toctree_same_values = set([v for v in reference_text_list if 1 < reference_text_list.count(v)])
         for node in toctree.traverse(nodes.reference):
-            anchorname = node.get('anchorname')
-            if node.get('internal') and anchorname:
-                new_anchorname = mapping.get(anchorname.replace("#", ""))
-                if new_anchorname:
-                    node['anchorname'] = '#' + new_anchorname
+            if node.get('internal') and node.get('anchorname'):
+                node_text = node.astext()
+                if node_text in toctree_same_values:
+                    original_anchorname = re.sub('^#', '', node.get('anchorname'))
+                    if delete_html_escape_string(node_text) not in original_anchorname:
+                        node['anchorname'] = '#{}-{}'.format(delete_html_escape_string(node.astext()), original_anchorname)
+                else:
+                    node['anchorname'] = '#{}'.format(delete_html_escape_string(node.astext()))
+
 
 class TableWrapperTransform(SphinxPostTransform):
     builder = ('html',)
