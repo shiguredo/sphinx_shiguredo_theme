56047b165ed4c1124053edfc3af740869918e9d4
Yuki Ito <yuito@shiguredo.jp>
2021-06-30 14:34:34 +0900

    メインコンテンツの下にスクロール用の空白を設置する

--- sphinx_shiguredo_theme/static/js/theme.js
+++ sphinx_shiguredo_theme/static/js/theme.js
@@ -1,46 +1,53 @@
-// left navi
-document.addEventListener("DOMContentLoaded", () => {
-    const scrollTarget = document.querySelector(".left-sidenavi-contents");
-    if (!scrollTarget) {
-        return;
-    }
-    const current = scrollTarget.querySelector("a.current");
-    if (!current) {
-        return;
-    }
-    const scrollTargetTop = scrollTarget?.getBoundingClientRect().top;
-    const currentTop = current.getBoundingClientRect().top;
-    scrollTarget.scrollTo(0, currentTop - scrollTargetTop);
-});
-window.addEventListener("scroll", (_) => {
-    const sections = document.querySelectorAll(".section");
-    // 現在のスクロール上部にある section を探す
-    const target = Array.from(sections).reduce((prevSection, currentSection) => {
-        const prevSectionRect = prevSection.getBoundingClientRect();
-        const currentSectionRect = currentSection.getBoundingClientRect();
-        if (0 < currentSectionRect.y || currentSectionRect.y < prevSectionRect.y) {
-            return prevSection;
+(function () {
+    function setCurrentRightSideNaviReference() {
+        const sections = document.querySelectorAll(".section");
+        // 現在のスクロール上部にある section を探す
+        const target = Array.from(sections).reduce((prevSection, currentSection) => {
+            const prevSectionRect = prevSection.getBoundingClientRect();
+            const currentSectionRect = currentSection.getBoundingClientRect();
+            if (0 < currentSectionRect.y || currentSectionRect.y < prevSectionRect.y) {
+                return prevSection;
+            }
+            return currentSection;
+        });
+        if (!target) {
+            return;
         }
-        return currentSection;
+        // 右コンテンツナビの対象リンクに current class を設定する
+        const rightSidenavi = document.querySelector(".right-sidenavi-contents");
+        if (!rightSidenavi) {
+            return;
+        }
+        const currentContent = rightSidenavi.querySelector("a.current");
+        if (currentContent) {
+            currentContent.classList.remove("current");
+        }
+        const prevContent = rightSidenavi.querySelector(`a[href="#${target.id}"]`);
+        if (!prevContent) {
+            return;
+        }
+        if (prevContent.classList.contains("current")) {
+            return;
+        }
+        prevContent.classList.add("current");
+    }
+    document.addEventListener("DOMContentLoaded", () => {
+        // left navi
+        const scrollTarget = document.querySelector(".left-sidenavi-contents");
+        if (!scrollTarget) {
+            return;
+        }
+        const current = scrollTarget.querySelector("a.current");
+        if (!current) {
+            return;
+        }
+        const scrollTargetTop = scrollTarget?.getBoundingClientRect().top;
+        const currentTop = current.getBoundingClientRect().top;
+        scrollTarget.scrollTo(0, currentTop - scrollTargetTop);
+        // right navi
+        setCurrentRightSideNaviReference();
     });
-    if (!target) {
-        return;
-    }
-    // 右コンテンツナビの対象リンクに current class を設定する
-    const rightSidenavi = document.querySelector(".right-sidenavi-contents");
-    if (!rightSidenavi) {
-        return;
-    }
-    const prevContent = rightSidenavi.querySelector(`a[href="#${target.id}"]`);
-    if (!prevContent) {
-        return;
-    }
-    if (prevContent.classList.contains("current")) {
-        return;
-    }
-    const currentContent = rightSidenavi.querySelector("a.current");
-    if (currentContent) {
-        currentContent.classList.remove("current");
-    }
-    prevContent.classList.add("current");
-});
+    window.addEventListener("scroll", (_) => {
+        setCurrentRightSideNaviReference();
+    });
+})();
