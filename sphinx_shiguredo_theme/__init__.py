5174c5ffe4a4c602ce7b1f2588c383858eb1b49d
max747 <takanori.kojima@gmail.com>
2024-03-10 23:35:43 +0900

    hase-based node-ID の利用は id0 のような id で始まる連番のケースに限定する

--- sphinx_shiguredo_theme/__init__.py
+++ sphinx_shiguredo_theme/__init__.py
@@ -6,6 +6,8 @@ import re
 from docutils import nodes
 from sphinx.transforms.post_transforms import SphinxPostTransform
 
+ANCHOR_ID_PATTERN = re.compile(r"#id[0-9]+")
+
 
 def on_doctree_resolved(app, doctree, docname):
     # add hash-based node-ID to sections
@@ -18,16 +20,20 @@ def on_doctree_resolved(app, doctree, docname):
         for node_id in node["ids"]:
             mapping[node_id] = new_id
         node["ids"].insert(0, new_id)
+
     # use hash-based node-IDs at local reference
     for node in doctree.traverse(nodes.reference):
         refid = node.get("refid")
         if refid in mapping:
             node["refid"] = mapping.get(refid)
+
     # use hash-based node-IDs at toctrees
     for _, toctree in app.env.tocs.items():
         sequences = defaultdict(int)
         for node in toctree.traverse(nodes.reference):
-            if node.get("internal") and node.get("anchorname"):
+            anchorname = node.get("anchorname")
+            # hase-based node-ID の利用は id0 のような id で始まる連番のケースに限定する
+            if node.get("internal") and anchorname and ANCHOR_ID_PATTERN.match(anchorname):
                 text = node.astext()
                 sequences[text] += 1
                 new_id = sha1("{}-{}".format(text, sequences[text]).encode("utf-8")).hexdigest()[:6]
