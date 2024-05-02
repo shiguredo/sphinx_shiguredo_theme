03b31e7953604e272f42f06f333ed91cbdc91050
voluntas <nakai@shiguredo.jp>
2024-05-02 09:42:07 +0900

    ruff format

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='sphinx_shiguredo_theme',
-    version='2022.1.0',
+    name="sphinx_shiguredo_theme",
+    version="2022.1.0",
     packages=find_namespace_packages(
-        include=['sphinx_shiguredo_theme', 'sphinx_shiguredo_theme.*']
+        include=["sphinx_shiguredo_theme", "sphinx_shiguredo_theme.*"]
     ),
     entry_points={
-        'sphinx.html_themes': [
-            'sphinx_shiguredo_theme = sphinx_shiguredo_theme',
+        "sphinx.html_themes": [
+            "sphinx_shiguredo_theme = sphinx_shiguredo_theme",
         ]
     },
     include_package_data=True,
     package_data={
-        'sphinx_shiguredo_theme': [
-            'theme.conf',
-            '*.html',
-            'static/css/*.css',
-            'static/js/*.js',
+        "sphinx_shiguredo_theme": [
+            "theme.conf",
+            "*.html",
+            "static/css/*.css",
+            "static/js/*.js",
         ]
     },
 )
